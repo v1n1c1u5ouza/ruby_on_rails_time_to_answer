30b95a74c3f0922ba8817c3f6c0e19ffe8322706
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2023-01-24 11:47:58 -0300

    Fix: Mispeling os var

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,9 +6,9 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
    @admin = Admin.find(params[:id])
   end
   def update
-    admin = Admin.find(params[:id])
+    @admin = Admin.find(params[:id])
     params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-    if admin.update(params_admin)
+    if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
