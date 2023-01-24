08250003d27e030d8e22b49c600756cf79b0c73b
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2023-01-24 11:07:40 -0300

    Feat: update admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit]
+    resources :admins, only: [:index, :edit, :update]
   end
+  
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
   namespace :site do
     get 'welcome/index'
   end
-  namespace :users_backoffice do
-    get 'welcome/index'
-  end
-  namespace :admins_backoffice do
-    get 'welcome/index'
-  end
+
   devise_for :admins
   devise_for :users
   
