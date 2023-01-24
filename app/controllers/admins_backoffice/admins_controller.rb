02113be1000e6dfb9b8c1012803e8d2108872378
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2023-01-24 12:08:32 -0300

    Feat: password change

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,8 +6,13 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
    @admin = Admin.find(params[:id])
   end
   def update
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end  
+
     @admin = Admin.find(params[:id])
     params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
