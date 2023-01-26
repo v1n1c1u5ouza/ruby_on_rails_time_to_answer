2647fcdc6b2787dd8058a61b4818d88fcca87626
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2023-01-26 08:33:11 -0300

    Feat: Helper to translate fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def translate_field( object = nil, method = nil )
+        (object && method) ? object.model.human_attribute_name(method) : "Informe o parâmetro corretamente!!!"
+    end
 end
